4179e67b08edaab2f5a13dad2b29d0c1e63878a3
Adam Hart <adamhayhart@gmail.com>
2021-02-06 05:51:21 -0800

    LoopyLighthouse BizzBuzz quiz success.

---
+++ loopyLighthouse.js
@@ -0,0 +1,22 @@
+
+for (let num = 100; num <= 200; num++) {
+  if (num % 12 === 0) console.log("LoopyLighthouse"); 
+  else if (num % 3 === 0) console.log("Loopy");
+  else if (num % 4 === 0) console.log("Lighthouse");
+  else console.log(num);
+
+  
+}
+
+/* var x = 1;
+
+while (x <= 20) {
+    // check divisibility
+    if (x % 15 === 0) console.log("JuliaJames");
+    else if (x % 3 === 0) console.log("Julia");
+    else if (x % 5 === 0) console.log("James");
+    else (console.log(x));
+    x = x + 1;
+    // print Julia, James, or JuliaJames
+    // increment x
+} */
