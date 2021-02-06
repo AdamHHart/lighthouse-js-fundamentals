0b0f822a0f36e997b45ed66ac18383e6417e53b4
Adam Hart <adamhayhart@gmail.com>
2021-02-06 05:31:25 -0800

    Using a while loop to repeat a cheesy chorus in a pop song.

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's groove! \n Let's move! \n Let's Party!";
+let repeat = 1;
+while (repeat < 10) {
+  if (repeat > 5){
+    console.log("*change key*")
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
