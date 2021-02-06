91f0a84f0464656ae03ab1e357cb6a78973fb921
Adam Hart <adamhayhart@gmail.com>
2021-02-06 15:09:25 -0800

    using conditionals if else to determine school grade

--- conditionals.js
+++ conditionals.js
@@ -1,31 +1,26 @@
-/*
 
-const isCitizen = true;
-const age = 26;
-let temperature = -50;
-let raining = false;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
-
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
-}
-
-if (!raining) {
-  console.log("Leave your umbrella at home!");
-} */
-let age = 12;
-
-
-  function whichSchool(age) {
-    if (age <= 12){
-      console.log("Elementary School")
-    } else if (age >=13 && age <= 18){
-      console.log("Secondary School")
-    } else {
-      console.log("Lighthouse Labs")
-    }
+const whichSchool  = function (age) {
+  if (age <= 12){
+    return("Elementary School")
+  } else if (age >= 13 && age <= 18) {
+    return("Secondary School")
+  } else {
+    return("Lighthouse Labs")
   }
+} 
 
+console.log(whichSchool(18));
+
+
+/*
+const whichSchool = function (age) {
+  if (age < 13) {
+    return “Elementary School”;
+  } else if (age >= 13 && age <= 18) {
+    return “Secondary School”;
+  } else {
+    return “Lighthouse Labs”;
+  }
+}
+console.log(whichSchool(12));
+*/
